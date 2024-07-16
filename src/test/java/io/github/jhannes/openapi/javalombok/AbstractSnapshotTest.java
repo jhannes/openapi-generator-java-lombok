7b049011c9ac95338712694bd5fe30299b1163c4
Johannes Brodwall <johannes@brodwall.com>
2024-07-16 21:09:41 +0200

    Initial test (fails)

---
+++ src/test/java/io/github/jhannes/openapi/javalombok/AbstractSnapshotTest.java
@@ -0,0 +1,65 @@
+package io.github.jhannes.openapi.javalombok;
+
+
+import org.openapitools.codegen.config.CodegenConfigurator;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class AbstractSnapshotTest {
+    public static final Path SNAPSHOT_ROOT = Paths.get("snapshotTests");
+    public static final Path LOCAL_SNAPSHOT_ROOT = Paths.get("localSnapshotTests");
+
+    protected static CodegenConfigurator createConfigurator(String modelName, Path input, Path outputDir) {
+        return createBaseConfigurator(input, outputDir)
+                .setModelNameSuffix("Dto")
+                .addAdditionalProperty("hideGenerationTimestamp", "true")
+                .setGeneratorName("java-lombok")
+                .setPackageName("io.github.jhannes.openapi." + modelName);
+    }
+
+    static CodegenConfigurator createBaseConfigurator(Path input, Path outputDir) {
+        return new CodegenConfigurator()
+                .setInputSpec(getInputSpec(input))
+                .setOutputDir(outputDir.toString());
+    }
+
+    private static String getInputSpec(Path input) {
+        String spec = input.toString();
+        try {
+            if (input.getFileName().toString().endsWith(".link")) {
+                String path = Files.readAllLines(input).get(0);
+                if (path.matches("https?://.*")) {
+                    spec = path;
+                } else {
+                    spec = Paths.get(path).toString();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return spec.replaceAll("\\\\", "/");
+    }
+
+    static void cleanDirectory(Path directory) throws IOException {
+        if (Files.isDirectory(directory)) {
+            try (Stream<Path> walk = Files.walk(directory)) {
+                walk.sorted(Comparator.reverseOrder())
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+            }
+        }
+    }
+
+    protected static String getModelName(Path file) {
+        String filename = file.getFileName().toString();
+        int lastDot = filename.lastIndexOf('.');
+        return lastDot < 0 ? filename : filename.substring(0, lastDot);
+    }
+
+}
---
+++ src/test/java/io/github/jhannes/openapi/javalombok/CompilerTest.java
@@ -0,0 +1,106 @@
+package io.github.jhannes.openapi.javalombok;
+
+import org.junit.jupiter.api.DynamicContainer;
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.TestFactory;
+import org.openapitools.codegen.ClientOptInput;
+import org.openapitools.codegen.DefaultGenerator;
+import org.openapitools.codegen.config.CodegenConfigurator;
+
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static io.github.jhannes.openapi.javalombok.AbstractSnapshotTest.cleanDirectory;
+import static io.github.jhannes.openapi.javalombok.AbstractSnapshotTest.createConfigurator;
+import static io.github.jhannes.openapi.javalombok.AbstractSnapshotTest.getModelName;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.DynamicContainer.dynamicContainer;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+public class CompilerTest {
+
+    @TestFactory
+    Stream<DynamicNode> outputsFromSpecsShouldCompile() throws IOException {
+        List<DynamicNode> testSuites = new ArrayList<>();
+        testSuites.add(compileSpec(AbstractSnapshotTest.SNAPSHOT_ROOT));
+        if (Files.isDirectory(AbstractSnapshotTest.LOCAL_SNAPSHOT_ROOT)) {
+            testSuites.add(compileSpec(AbstractSnapshotTest.LOCAL_SNAPSHOT_ROOT));
+        }
+        return testSuites.stream();
+    }
+
+    DynamicNode compileSpec(Path testDir) throws IOException {
+        Path inputDir = testDir.resolve("input");
+        return dynamicContainer(
+                "Output should compile: " + testDir,
+                Files.list(inputDir)
+                        .filter(p -> p.toFile().isFile())
+                        .map(this::createTestFromSpec)
+        );
+    }
+
+    public DynamicNode createTestFromSpec(Path spec) {
+        String modelName = getModelName(spec);
+        Path outputDir = spec.getParent().getParent().resolve("compile").resolve(modelName);
+        var configurator = createConfigurator(modelName, spec, outputDir);
+        return createTestFromSpec(spec, configurator, outputDir);
+    }
+
+    static DynamicContainer createTestFromSpec(Path spec, CodegenConfigurator configurator, Path outputDir) {
+        return dynamicContainer("Compile " + spec, Arrays.asList(
+                dynamicTest("Clean " + spec, () -> cleanDirectory(outputDir)),
+                dynamicTest("Generate " + spec, () -> generate(configurator)),
+                dynamicTest("javac " + spec, () -> compile(outputDir))
+        ));
+    }
+
+    static void generate(CodegenConfigurator configurator) {
+        final ClientOptInput clientOptInput = configurator.toClientOptInput();
+        DefaultGenerator generator = new DefaultGenerator();
+        generator.opts(clientOptInput).generate();
+    }
+
+
+    static void compile(Path path) throws IOException {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        try (StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null)) {
+
+            List<Path> files = Files.find(
+                    path.resolve("src/main/java"),
+                    999,
+                    (p, fa) -> p.getFileName().toString().endsWith(".java") && fa.isRegularFile()
+            ).collect(Collectors.toList());
+
+            List<String> options = List.of("-Xlint:deprecation", "-d", path.resolve("target/compile").toString());
+            System.out.println("javac " + String.join(" " , options) + " " + files.stream().map(Path::toString).collect(Collectors.joining(" ")));
+
+            DiagnosticCollector<JavaFileObject> diagnosticListener = new DiagnosticCollector<>();
+            JavaCompiler.CompilationTask task = compiler.getTask(
+                    null,
+                    fileManager,
+                    diagnosticListener,
+                    List.of("-Xlint:deprecation", "-d", "target/testCompile/" + path.getFileName()),
+                    null,
+                    fileManager.getJavaFileObjectsFromPaths(files)
+            );
+            Boolean result = task.call();
+            if (!diagnosticListener.getDiagnostics().isEmpty()) {
+                fail(diagnosticListener.getDiagnostics().toString());
+            }
+            assertTrue(result);
+        }
+    }
+
+}
